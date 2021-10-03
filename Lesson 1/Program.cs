c03b617d105e14a6f796d812e915c1a122131334
ClareBurns <89960135+ClareBurns@users.noreply.github.com>
2021-10-03 11:12:34 +1100

    Initial Upload

---
+++ Lesson 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson_Activity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("And so we've started upon our road");
+            Console.WriteLine("Of learning C#, as we learn to code");
+        }
+    }
+}

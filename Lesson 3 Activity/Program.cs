978f8ac661ecf46730703087e6e1c9dd713d5a01
ClareBurns <89960135+ClareBurns@users.noreply.github.com>
2021-10-18 14:00:08 +1100

    Update Program.cs
    -Change double.Parse() to int.Parse() for month and year variables
    -Adding to Trim of $ and % 
    -adding information about meeting savings goal
    -rounding savings to 2 decimal places

--- Lesson 3 Activity/Program.cs
+++ Lesson 3 Activity/Program.cs
@@ -6,47 +6,52 @@ namespace Lesson_3_Activity
     {
         static void Main(string[] args)
         {
-            //calculating in monthly variable
+            Char[] toTrim = { '$', '%', ' ' };
+
+            //calculating in monthly variables
             Console.WriteLine("Please enter your initial deposit: ");
-            var initialDeposit = double.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter your monthly interest rate (in percentage): "); 
-            var monthlyInterestRate = double.Parse(Console.ReadLine());
-            monthlyInterestRate = monthlyInterestRate/100; 
-            Console.WriteLine("Please enter how many months you will save for: "); 
-            var month = double.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter your regular monthly deposits: "); 
-            var monthlyDeposits = double.Parse(Console.ReadLine()); 
-            Console.WriteLine(monthlyDeposits); 
+            var initialDeposit = double.Parse(Console.ReadLine().Trim(toTrim));
+            Console.WriteLine("Please enter your monthly interest rate (in percentage): ");
+            var monthlyInterestRate = double.Parse(Console.ReadLine().Trim(toTrim));
+            monthlyInterestRate = monthlyInterestRate / 100;
+            Console.WriteLine("Please enter how many months you will save for: ");
+            var month = int.Parse(Console.ReadLine().Trim(toTrim));
+            Console.WriteLine("Please enter your regular monthly deposits: ");
+            var monthlyDeposits = double.Parse(Console.ReadLine().Trim(toTrim));
+            Console.WriteLine(monthlyDeposits);
+
+            var savings = initialDeposit * Math.Pow((1 + monthlyInterestRate), month)
+            + (monthlyDeposits / monthlyInterestRate) * (Math.Pow((1 + monthlyInterestRate), month) - 1);
+
+            var savingsGoal = 2000;
+            var doesMeetSavingsGoal = savings >= savingsGoal;
 
 
-            var savings = initialDeposit*Math.Pow((1 + monthlyInterestRate), month) 
-            + (monthlyDeposits/monthlyInterestRate)*(Math.Pow((1 + monthlyInterestRate), month) - 1); 
+            var savingsRounded = Math.Round(savings, 2);
+            Console.WriteLine("Your calculated savings are: ");
+            Console.WriteLine($"${savingsRounded}");
+            Console.WriteLine("Does this meet the savings Goal?");
+            Console.WriteLine(doesMeetSavingsGoal);
 
-            Console.WriteLine(savings); 
-            
             //calculating in yearly variables
             Console.WriteLine("Please enter your initial deposit: ");
-            initialDeposit = double.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter your yearly interest rate (in percentage): "); 
-            var yearlyInterestRate = double.Parse(Console.ReadLine());
-            yearlyInterestRate = yearlyInterestRate/100; 
-            Console.WriteLine("Please enter how many years you will save for: "); 
-            var year = double.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter your regular monthly deposits: "); 
-            monthlyDeposits = double.Parse(Console.ReadLine()); 
-            Console.WriteLine(monthlyDeposits); 
+            initialDeposit = double.Parse(Console.ReadLine().Trim(toTrim));
+            Console.WriteLine("Please enter your yearly interest rate (in percentage): ");
+            var yearlyInterestRate = double.Parse(Console.ReadLine().Trim(toTrim));
+            yearlyInterestRate = yearlyInterestRate / 100;
+            Console.WriteLine("Please enter how many years you will save for: ");
+            var year = int.Parse(Console.ReadLine().Trim(toTrim));
+            Console.WriteLine("Please enter your regular monthly deposits: ");
+            monthlyDeposits = double.Parse(Console.ReadLine().Trim(toTrim));
+            Console.WriteLine(monthlyDeposits);
 
+            var savings2 = initialDeposit * Math.Pow((1 + yearlyInterestRate / 12), year * 12)
+            + (monthlyDeposits / (yearlyInterestRate / 12)) * (Math.Pow((1 + yearlyInterestRate / 12), year * 12) - 1);
 
-            var savings2 = initialDeposit*Math.Pow((1 + yearlyInterestRate/12), year*12) 
-            + (monthlyDeposits/(yearlyInterestRate/12))*(Math.Pow((1 + yearlyInterestRate/12), year*12) - 1); 
+            var savings2Rounded = Math.Round(savings2, 2);
+            Console.WriteLine("Your calculated savings are: ");
+            Console.WriteLine($"${savings2Rounded}");
 
-            Console.WriteLine(savings2); 
-
-
-            //var number3 = double.Parse(13); why does this not work?
-            //var a = int.Parse("15.0"); why does this not work?
-            // why 9 & 7, why not 9.0 and 7.0. Is 9 always an integer or can it be a double? 
-            // e.g. 1.2(double) + 0.8(double) = 2(integer) or 2(double)??
 
         }
     }

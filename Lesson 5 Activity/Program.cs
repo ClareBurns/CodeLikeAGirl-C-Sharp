f3431def554faa199191acd5498dea578af71f5a
ClareBurns <89960135+ClareBurns@users.noreply.github.com>
2021-11-05 16:51:24 +1100

    Updated program.cs

--- Lesson 5 Activity/Program.cs
+++ Lesson 5 Activity/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Lesson_5_Activity
@@ -8,15 +8,14 @@ namespace Lesson_5_Activity
         static void Main(string[] args)
         {
             //Defining lists 
-            var students = new List<string> { "Clare Burns", "Sofia Burns", "Gemma Burns" };
-            var courses = new List<string> { "Science", "Arts", "Commence", "IT" };
-            var studentClareBurns = new List<string> { "Science" };
-            var studentSofiaBurns = new List<string> { "Commence" };
-            var studentGemmaBurns = new List<string> { "Commence", "IT" };
-            var courseScience = new List<string> { "Clare Burns" };
-            var courseCommence = new List<string> { "Sofia Burns", "Gemma Burns" };
+            var students = new List<string> { "clare burns", "sofia burns", "gemma burns" };
+            var courses = new List<string> { "science", "arts", "commerce" };
+            var studentClareBurns = new List<string> { "science" };
+            var studentSofiaBurns = new List<string> { "commerce" };
+            var studentGemmaBurns = new List<string> { "commerce", "science" };
+            var courseScience = new List<string> { "clare burns", "gemma burns" };
+            var courseCommerce = new List<string> { "sofia burns", "gemma burns" };
             var courseArts = new List<string> { };
-            var courseIT = new List<string> { "Gemma Burns" };
 
             while (true)
             {
@@ -54,84 +53,227 @@ namespace Lesson_5_Activity
                         }
                         break;
                     case "student details":
-                        Console.WriteLine("What is the full name of the student you would like to");
+                        Console.WriteLine("What is the full name of the student you would like to get the details of?");
                         var studentName = Console.ReadLine().Trim().ToLower();
-                        if (studentName == "clare burns")
+                        if (students.Contains(studentName))
                         {
-                            Console.Write($"Clare Burns is enrolled in ");
-                            foreach (var course in studentClareBurns)
+                            switch (studentName)
                             {
-                                Console.WriteLine($"{course}");
+                                case "clare burns":
+                                    Console.Write($"Clare Burns is enrolled in ");
+                                    foreach (var course in studentClareBurns)
+                                    {
+                                        Console.WriteLine($"{course}");
+                                    }
+                                    break;
+                                case "sofia burns":
+                                    Console.Write($"Sofia Burns is enrolled in ");
+                                    foreach (var course in studentSofiaBurns)
+                                    {
+                                        Console.WriteLine($"{course}");
+                                    }
+                                    break;
+
+                                case "gemma burns":
+                                    Console.WriteLine($"Gemma Burns is enrolled in: ");
+                                    foreach (var course in studentGemmaBurns)
+                                    {
+                                        Console.WriteLine($"{course}");
+                                    }
+                                    break;
                             }
                         }
-                        if (studentName == "sofia burns")
+                        else
                         {
-                            Console.Write($"Sofia Burns is enrolled in ");
-                            foreach (var course in studentSofiaBurns)
-                            {
-                                Console.WriteLine($"{course}");
-                            }
-                        }
-                        if (studentName == "gemma burns")
-                        {
-                            Console.Write($"Gemma Burns is enrolled in ");
-                            foreach (var course in studentGemmaBurns)
-                            {
-                                Console.WriteLine($"{course}");
-                            }
+                            Console.WriteLine("Sorry the student you have entered is not in the database.");
                         }
                         break;
                     case "enrol":
-                        Console.WriteLine("How many students would you like to enrol today?");
-                        var enrollmentNumber = int.Parse(Console.ReadLine());
-                        for (var i = 1; i <= enrollmentNumber; i++)
+                        Console.WriteLine("Please enter the full name of the student you would like to enrol");
+                        var newStudent = Console.ReadLine().Trim().ToLower();
+                        while (!string.IsNullOrEmpty(newStudent))
                         {
-                            Console.WriteLine("Please enter the full name of the student you would like to enrol");
-                            var newStudent = Console.ReadLine();
-                            students.Add(newStudent);
-                            Console.WriteLine($"{newStudent} has been enrolled. Thank you!");
-                        }
-                        break;
-                    case "unenrol":
-                        Console.WriteLine("How many students would you like to unenrol today?");
-                        var unenrollmentNumber = int.Parse(Console.ReadLine());
-                        for (var i = 1; i <= unenrollmentNumber; i++)
-                        {
-                            Console.WriteLine("Please enter the full name of the student you would like to enrol");
-                            var removedStudent = Console.ReadLine();
-                            students.Remove(removedStudent);
-                            Console.WriteLine($"{removedStudent} has been unenrolled. Thank you!");
-                        }
-                        break;
-                    /*case "add course":
-                        Console.WriteLine("What is the full name of the student you would like to add courses for?");
-                        studentName = Console.ReadLine();
-                        while (true)
-                        {
-                            Console.WriteLine("What courses would you like to add? Type X when you are done adding courses.");
-                            var addedCourse = Console.ReadLine();
-                            if (addedCourse == "X")
+                            if (students.Contains(newStudent))
                             {
-                                break;
+                                Console.WriteLine("Sorry, this student is already enrolled");
+                            }
+                            else if (int.TryParse(newStudent, out int numberEnrolError))
+                            {
+                                Console.WriteLine("Sorry, you have entered numbers instead of a name. Please try again");
                             }
                             else
                             {
-                                var student{ } = Console.ReadLine();
+                                students.Add(newStudent);
+                                Console.WriteLine($"{newStudent} has been enrolled. Thank you!");
+                            }
+                            Console.WriteLine("If you wish to enrol another student please enter their name. Otherwise, press enter.");
+                            newStudent = Console.ReadLine().Trim().ToLower();
+                        }
+                        break;
+                    case "unenrol":
+                        Console.WriteLine("Please enter the full name of the student you would like to unenrol");
+                        var removedStudent = Console.ReadLine().Trim().ToLower();
+                        while (!string.IsNullOrEmpty(removedStudent))
+                        {
+                            if (students.Contains(removedStudent))
+                            {
                                 students.Remove(removedStudent);
                                 Console.WriteLine($"{removedStudent} has been unenrolled. Thank you!");
                             }
+                            else if (int.TryParse(removedStudent, out int numberUnenrolError))
+                            {
+                                Console.WriteLine("Sorry, you have entered numbers instead of a name. Please try again");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{removedStudent} is not currently enrolled.");
+                            }
+                            Console.WriteLine("If you wish to unenrol another student please enter their name. Otherwise, press enter.");
+                            removedStudent = Console.ReadLine().Trim().ToLower();
                         }
-                        break; */
+                        break;
+                    case "add course":
+                        Console.WriteLine("Please enter the name of the student you would like to add to the course");
+                        studentName = Console.ReadLine().Trim().ToLower();
+                        while (!string.IsNullOrEmpty(studentName))
+                        {
+                            if (!students.Contains(studentName))
+                            {
+                                Console.WriteLine("Sorry the student you have entered is not in the database. Please enrol this student before adding their courses.");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Please enter the course you would like to enrol {studentName} to");
+                                var courseName = Console.ReadLine().Trim().ToLower();
+                                while (!string.IsNullOrEmpty(courseName))
+                                {
+                                    if (!courses.Contains(courseName))
+                                    {
+                                        Console.WriteLine("Sorry the course you have entered is not offered at the university.");
+                                    }
+                                    else
+                                    {
+
+                                        if (courseName == "science" && !courseScience.Contains(studentName) && courseScience.Count <= 2)
+                                        {
+                                            courseScience.Add(studentName);
+                                        }
+                                        else if (courseName == "commerce" && !courseCommerce.Contains(studentName) && courseCommerce.Count <= 2)
+                                        {
+                                            courseCommerce.Add(studentName);
+                                        }
+                                        else if (courseName == "arts" && !courseArts.Contains(studentName) && courseArts.Count <= 2)
+                                        {
+                                            courseArts.Add(studentName);
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine($"Sorry, {courseName} is full! Try again next year.");
+                                            break;
+                                        }
+
+                                        if (studentName == "clare burns" && !studentClareBurns.Contains(courseName))
+                                        {
+                                            studentClareBurns.Add(courseName);
+                                            Console.WriteLine($"{studentName} is now enrolled in {courseName}");
+                                        }
+                                        else if (studentName == "sofia burns" && !studentSofiaBurns.Contains(courseName))
+                                        {
+                                            studentSofiaBurns.Add(courseName);
+                                            Console.WriteLine($"{studentName} is now enrolled in {courseName}");
+                                        }
+                                        else if (studentName == "gemma burns" && !studentGemmaBurns.Contains(courseName))
+                                        {
+                                            studentGemmaBurns.Add(courseName);
+                                            Console.WriteLine($"{studentName} is now enrolled in {courseName}");
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine($"Sorry, {studentName} is already enrolled in {courseName}");
+                                        }
+                                    }
+                                    Console.WriteLine($"If you wish to add {studentName} to another course please enter the course. Otherwise, press enter.");
+                                    courseName = Console.ReadLine().Trim().ToLower();
+                                }
+                            }
+                            Console.WriteLine("If you wish to add courses to another student please enter their name. Otherwise, press enter.");
+                            studentName = Console.ReadLine().Trim().ToLower();
+                        }
+                        break;
+                    case "remove course":
+                        Console.WriteLine("Please enter the name of the student you would like to remove from a course");
+                        studentName = Console.ReadLine().Trim().ToLower();
+                        while (!string.IsNullOrEmpty(studentName))
+                        {
+                            if (!students.Contains(studentName))
+                            {
+                                Console.WriteLine("Sorry the student you have entered is not in the database.");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Please enter the course you would like to remove {studentName} from");
+                                var courseName = Console.ReadLine().Trim().ToLower();
+                                while (!string.IsNullOrEmpty(courseName))
+                                {
+                                    if (!courses.Contains(courseName))
+                                    {
+                                        Console.WriteLine("Sorry the course you have entered is not offered at the university.");
+                                    }
+                                    else
+                                    {
+
+                                        if (courseName == "science" && courseScience.Contains(studentName))
+                                        {
+                                            courseScience.Remove(studentName);
+                                        }
+                                        else if (courseName == "commerce" && !courseCommerce.Contains(studentName) && courseCommerce.Count <= 2)
+                                        {
+                                            courseCommerce.Remove(studentName);
+                                        }
+                                        else if (courseName == "arts" && courseArts.Contains(studentName))
+                                        {
+                                            courseArts.Remove(studentName);
+                                        }
+                                        if (studentName == "clare burns" && studentClareBurns.Contains(courseName))
+                                        {
+                                            studentClareBurns.Remove(courseName);
+                                            Console.WriteLine($"{studentName} is now removed from {courseName}");
+                                        }
+                                        else if (studentName == "sofia burns" && studentSofiaBurns.Contains(courseName))
+                                        {
+                                            studentSofiaBurns.Remove(courseName);
+                                            Console.WriteLine($"{studentName} is now removed in {courseName}");
+                                        }
+                                        else if (studentName == "gemma burns" && studentGemmaBurns.Contains(courseName))
+                                        {
+                                            studentGemmaBurns.Remove(courseName);
+                                            Console.WriteLine($"{studentName} is now removed in {courseName}");
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine($"Sorry, {studentName} is already unenrolled from {courseName}");
+                                        }
+                                    }
+                                    Console.WriteLine($"If you wish to remove {studentName} from another course please enter the course. Otherwise, press enter.");
+                                    courseName = Console.ReadLine().Trim().ToLower();
+                                }
+
+                            }
+                            Console.WriteLine("If you wish to remove courses from another student please enter their name. Otherwise, press enter.");
+                            studentName = Console.ReadLine().Trim().ToLower();
+                        }
+                        break;
                     default:
                         Console.WriteLine("Sorry, the database does not have that function.");
                         break;
+
+
                 }
 
-                //need to use a for each loop even if just one variable? 
-                //how do you make a list using a for loop? --> make an empty array
+                Console.WriteLine("------------------------------------------------------------------------------");
             }
         }
     }
-
 }
 
+// how do variables move from if statements to switch statements etcccc. 

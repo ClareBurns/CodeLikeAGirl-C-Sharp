6891cf9df0c16fe511cbee7d6b92f15c02adebfe
ClareBurns <clare.burns@ozemail.com.au>
2021-11-07 11:34:48 +1100

    Created new console

---
+++ Lesson 6 Activity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson_6_Activity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

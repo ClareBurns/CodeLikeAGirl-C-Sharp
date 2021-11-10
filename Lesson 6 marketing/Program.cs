52b039429c015cadb90cde93d3cbcb9629b11349
ClareBurns <clare.burns@ozemail.com.au>
2021-11-10 14:00:49 +1100

    Create foreach loop

--- Lesson 6 marketing/Program.cs
+++ Lesson 6 marketing/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Lesson_6_marketing
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var names = new List<string> { "Elizabeth", "Charles", "William", "George" };
+            foreach (var name in names)
+            {
+                SubscriptionThankYou(name);
+            }
+
+        }
+
+        static void SubscriptionThankYou(string name)
+        {
+            Console.WriteLine($"Dear {name}");
+            Console.WriteLine("Thank you fo subscribing to our newsletter");
+            Console.WriteLine("Kind Regards");
+            Console.WriteLine("The Code Like a Girl Team");
         }
     }
 }
+

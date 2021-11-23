153e19158831790d19dcfca60c048a2c1f3b93eb
ClareBurns <clare.burns@ozemail.com.au>
2021-11-23 13:27:24 +1100

    Add book option

--- Lesson 7 Activity/Program.cs
+++ Lesson 7 Activity/Program.cs
@@ -20,17 +20,6 @@ namespace Lesson_7_Activity
 
                 Console.WriteLine("---------------------------------------------------------------");
 
-
-                var members = DeclareMembers();
-
-                foreach (var member in members)
-                {
-                    member.DisplayMemberDetails();
-                    foreach (var book in member.readingList)
-                    {
-                        book.ShareBookDetails();
-                    }
-                }
             }
         }
 
@@ -39,15 +28,17 @@ namespace Lesson_7_Activity
             Console.WriteLine("Welcome to the book club!");
             Console.WriteLine("To see a list of the recommended books type BOOKS");
             Console.WriteLine("To see a list of the members type MEMBERS");
-            Console.WriteLine("To add a new member type NEW MEMBER");
+            Console.WriteLine("Each see each member's favourite book type FAV BOOK");
+            Console.WriteLine("Each see each member's reading list type READING LIST");
             Console.WriteLine("To add a new book type NEW BOOK");
+            Console.WriteLine("To add a new member type NEW MEMBER");
             Console.WriteLine("To exit the program type X");
         }
 
         static void RespondtoUserCommands(string userInput)
         {
             var recommendedBooks = DeclareBooks();
-            var members = DeclareMembers();
+            var members = DeclareMembers(recommendedBooks);
 
             switch (userInput)
             {
@@ -55,14 +46,19 @@ namespace Lesson_7_Activity
                     DisplayBooks(recommendedBooks);
                     break;
                 case "members":
-                    Console.WriteLine("members");
                     DisplayMembers(members);
                     break;
-                case "addmembers":
-                    Console.WriteLine("add members");
+                case "favbook":
+                    DisplayFavBook(members);
                     break;
-                case "addbooks":
-                    Console.WriteLine("add books");
+                case "readinglist":
+                    DisplayReadingList(members);
+                    break;
+                case "newbook":
+                    AddBook(recommendedBooks);
+                    break;
+                case "newmember":
+                    Console.WriteLine("add members");
                     break;
                 default:
                     Console.WriteLine("Sorry we don't have the option.");
@@ -89,35 +85,47 @@ namespace Lesson_7_Activity
                 author = "Salman Rushdie"
             };
 
-            var recommendedBooks = new List<Book> { Book1, Book2, Book3 };
+            var Book4 = new Book
+            {
+                title = "Harry Potter",
+                author = "J.K Rowling"
+            };
+
+            var Book5 = new Book
+            {
+                title = "The Book Thief",
+                author = "Markus Zusak"
+            };
+
+            var recommendedBooks = new List<Book> { Book1, Book2, Book3, Book4, Book5 };
 
             return recommendedBooks;
         }
 
-        static List<Member> DeclareMembers()
+        static List<Member> DeclareMembers(List<Book> recommendedBooks)
         {
             var member1 = new Member
             {
                 firstName = "Clare",
                 secondName = "Burns",
-                favouriteBook = "I know why the caged bird sings",
-                readingList = new List<Book> { new Book() }
+                favouriteBook = recommendedBooks[0],
+                readingList = new List<Book> { recommendedBooks[1] }
             };
 
             var member2 = new Member
             {
                 firstName = "Sofia",
                 secondName = "Burns",
-                favouriteBook = "Ruby Redfort",
-                readingList = new List<Book> { new Book() }
+                favouriteBook = recommendedBooks[1],
+                readingList = new List<Book> { recommendedBooks[0], recommendedBooks[4] }
             };
 
             var member3 = new Member
             {
                 firstName = "Penny",
                 secondName = "Burns",
-                favouriteBook = "Midnight Children",
-                readingList = new List<Book> { new Book() }
+                favouriteBook = recommendedBooks[2],
+                readingList = new List<Book> { recommendedBooks[0], recommendedBooks[1], recommendedBooks[3] }
             };
 
             var members = new List<Member> { member1, member2, member3 };
@@ -143,6 +151,57 @@ namespace Lesson_7_Activity
             }
         }
 
+        static void DisplayReadingList(List<Member> members)
+        {
+            Console.WriteLine("The reading list for each member is: ");
+            foreach (var member in members)
+            {
+                Console.WriteLine($"For {member.firstName} {member.secondName}: ");
+                foreach (var book in member.readingList)
+                {
+                    Console.WriteLine($"\t {book.title} by {book.author}");
+                }
+            }
+        }
+
+        static void DisplayFavBook(List<Member> members)
+        {
+            foreach (var member in members)
+            {
+                member.DisplayMemberDetails();
+            }
+        }
+
+        static void AddBook(List<Book> recommendedBooks)
+        {
+            Console.WriteLine("What is the title of the book you would like to add: ");
+            var newTitle = Console.ReadLine();
+            Console.WriteLine("What is the author of the book you would like to add: ");
+            var newAuthor = Console.ReadLine();
+            var alreadyAdded = false;
+            foreach (var book in recommendedBooks)
+            {
+                if (book.title == newTitle && book.author == newAuthor)
+                {
+                    alreadyAdded = true;
+                    break;
+                }
+            }
+
+            if (alreadyAdded)
+            {
+                Console.WriteLine("This book is already on the recommended book list.");
+            }
+
+            else
+            {
+                var newBook = new Book() { title = newTitle, author = newAuthor };
+                recommendedBooks.Add(newBook);
+                Console.WriteLine($"{newBook.title} by {newBook.author} has been added to the recommended book list");
+            }
+
+        }
+
     }
 
     public class Book
@@ -160,13 +219,13 @@ namespace Lesson_7_Activity
     {
         public string firstName;
         public string secondName;
-        public string favouriteBook;
+        public Book favouriteBook;
 
         public List<Book> readingList;
 
         public void DisplayMemberDetails()
         {
-            Console.WriteLine($"{firstName} {secondName}'s favourite book is {favouriteBook}");
+            Console.WriteLine($"{firstName} {secondName}'s favourite book is {favouriteBook.title} by {favouriteBook.author}");
         }
     }
 

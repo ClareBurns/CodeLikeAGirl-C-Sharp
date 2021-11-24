8a99079ac77d265ec18be3ca06de44c1599fbe3d
ClareBurns <clare.burns@ozemail.com.au>
2021-11-24 17:32:10 +1100

    Initial Upload

---
+++ Lesson 8 Film/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson_8_Film
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var film = new Film("Toy Story", 1994);
+            Console.WriteLine(film.Title);
+        }
+    }
+
+    public class Film
+    {
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+
+        public Film(string title, int releaseYear)
+        {
+            Title = title;
+            ReleaseYear = releaseYear;
+        }
+    }
+}
